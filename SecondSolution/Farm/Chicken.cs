99884f106443b4d1de4cf43b44a37e87c9373773
Don3008 <don3008@yandex.ru>
2019-03-19 22:13:08 +0300

    Добавлена реализация проекта с поддержкой ООП

---
+++ SecondSolution/Farm/Chicken.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    class Chicken
+    {
+        private readonly int maxHealth = 5;
+
+        public int Eggs { get; private set; }
+        public bool IsLive { get; private set; }
+        public int Health { get; private set; }
+        public int MaxHealth
+        {
+            get
+            {
+                return maxHealth;
+            }
+            private set
+            {
+                MaxHealth = maxHealth;
+            }
+        }
+
+        public Chicken()
+        {
+            Health = 3;
+            Eggs = 0;
+        }
+
+        public void Eat(int forage)
+        {
+            Health += forage;
+            Console.WriteLine("Здоровье курицы после еды {0}", Health);
+            MakeEgg(forage);
+        }
+
+        public void MakeEgg(int forage)
+        {
+            Eggs += forage;
+        }
+
+        public void GiveEgg()
+        {
+            Eggs--;
+        }
+
+        public bool Death()
+        {
+            Health--;
+            if (Health <= 0)
+            {
+                Console.WriteLine("Курица умерла:(");
+                IsLive = false;
+            }
+            else
+            {
+                IsLive = true;
+            }
+            return IsLive;
+        }
+    }
+}
---
+++ SecondSolution/Farm/Game.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    enum Action
+    {
+        Feed = 1,
+        TakeEgg,
+        DoNothing
+    }
+    class Game
+    {
+        static Chicken chicken = new Chicken();
+        static int sumOfEggs;
+
+        static void Main(string[] args)
+        {
+            bool isAlive = true;
+            Console.WriteLine("Игра Ферма");
+            while (isAlive)
+            {
+                Welcome();
+                int input = Input();
+                ChooseAction(input);
+                isAlive = chicken.Death();
+                Console.WriteLine($"У вас {sumOfEggs} яиц");
+            }
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("Выберете действие: \n {0} - Покормить \n {1} - Забрать яйцо" +
+            "\n {2} - Ничего не делать", (int)Action.Feed, (int)Action.TakeEgg,
+            (int)Action.DoNothing);
+        }
+
+        static int Input()
+        {
+            int input = int.Parse(Console.ReadLine());
+            return input;
+        }
+
+        static void ChooseAction(int input)
+        {
+            Action action;
+            action = (Action)input;
+            switch (action)
+            {
+                case Action.Feed:
+                    Feed();
+                    break;
+                case Action.TakeEgg:
+                    TakeEgg();
+                    break;
+                case Action.DoNothing:
+                    DoNothing();
+                    break;
+                default:
+                    Console.WriteLine("Такого действия не существует!");
+                    break;
+            }
+        }
+
+        static void Feed()
+        {
+            Console.WriteLine("Текущее здоровье курицы: {0} из {1}", chicken.Health, chicken.MaxHealth);
+            int maxForage = chicken.MaxHealth - chicken.Health;
+            Console.WriteLine("Вы можете покормить курицу на: ");
+            for (int i = maxForage; i > 0; i--)
+            {
+                Console.WriteLine("- {0} ед.", i);
+            }
+            while (true)
+            {
+                int forage = Input();
+                if (forage > maxForage)
+                {
+                    Console.WriteLine("Не нужно перекармливать курицу!");
+                }
+                else
+                {
+                    chicken.Eat(forage);
+                    break;
+                }
+            }
+        }
+
+        static void TakeEgg()
+        {
+            if (chicken.Eggs > 0)
+            {
+                Console.WriteLine("Вы забрали одно яйцо");
+                sumOfEggs++;
+                chicken.GiveEgg();
+                Console.WriteLine("Оставшихся яиц у курицы: {0}", chicken.Eggs);
+            }
+            else
+            {
+                Console.WriteLine("Курица еще не снесла ни одного яйца");
+            }
+            Console.WriteLine("Здоровье курицы: {0} ед.", chicken.Health);
+        }
+
+        static void DoNothing()
+        {
+            Console.WriteLine("Курим в сторонке...");
+            Console.WriteLine("Здоровье курицы: {0} ед.", chicken.Health);
+        }
+    }
+}
